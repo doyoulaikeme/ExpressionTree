a6d70a9d0e7aa71e46fec1a71978e0b92e80af96
强 梁 <673615750@qq.com>
2021-01-20 17:29:13 +0800

    添加查看表达式树插件。

--- ExpressionTree/Program.cs
+++ ExpressionTree/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +10,16 @@ namespace ExpressionTree
     class Program
     {
         /// <summary>
-        /// 
+        /// 表达式树
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Console.WriteLine("***********表达式树******************");
+
+            Expression<Func<int, bool>> pression = p => p > 100;
+
+            Console.ReadKey();
 
         }
     }
